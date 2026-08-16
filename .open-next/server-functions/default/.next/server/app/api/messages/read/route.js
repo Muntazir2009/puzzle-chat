9beaf822c88ef6298a6206c868eff31399940641
z-ai-dev <z-ai-dev@users.noreply.github.com>
2026-08-16 06:07:51 +0000

    9db6337e-bf8c-474f-beea-000439e688d7

---
+++ .open-next/server-functions/default/.next/server/app/api/messages/read/route.js
@@ -0,0 +1,9 @@
+var R=require("../../../../chunks/[turbopack]_runtime.js")("server/app/api/messages/read/route.js")
+R.c("server/chunks/[root-of-the-server]__fa025ebc._.js")
+R.c("server/chunks/[root-of-the-server]__f408c708._.js")
+R.c("server/chunks/node_modules_zod_v4_classic_external_fa90cebf.js")
+R.c("server/chunks/[root-of-the-server]__38d471a4._.js")
+R.c("server/chunks/_974e6aeb._.js")
+R.c("server/chunks/_next-internal_server_app_api_messages_read_route_actions_0582009b.js")
+R.m(19855)
+module.exports=R.m(19855).exports
---
+++ .open-next/server-functions/default/.next/server/app/api/messages/send/route.js
@@ -0,0 +1,9 @@
+var R=require("../../../../chunks/[turbopack]_runtime.js")("server/app/api/messages/send/route.js")
+R.c("server/chunks/[root-of-the-server]__c2b6e4d1._.js")
+R.c("server/chunks/[root-of-the-server]__f408c708._.js")
+R.c("server/chunks/node_modules_zod_v4_classic_external_fa90cebf.js")
+R.c("server/chunks/[root-of-the-server]__38d471a4._.js")
+R.c("server/chunks/_974e6aeb._.js")
+R.c("server/chunks/_next-internal_server_app_api_messages_send_route_actions_98cc16e6.js")
+R.m(11121)
+module.exports=R.m(11121).exports
